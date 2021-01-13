5f573b8f01dd54f27f98794b030e19cb72d5fd30
roman89ua <roman89ua@gmail.com>
2021-01-14 00:13:30 +0200

    flex direction, alignItems, alignSelf, justifyContent,

--- App.js
+++ App.js
@@ -32,24 +32,32 @@ export default function App() {
       style={{
         backgroundColor: "#fff",
         flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          flex: 2,
+          width: 100,
+          height: 300,
+          alignSelf: "flex-start",
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
-          flex: 1,
+          width: 100,
+          height: 200,
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
-          flex: 1,
+          width: 100,
+          height: 100,
+          alignSelf: "flex-end",
         }}
       />
     </View>
